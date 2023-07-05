92db67a2f91d689aab601bfdef384a54e32fde45
wickles <wickles137@gmail.com>
2023-07-04 21:17:47 -0700

    cask: macports 2.8.1

--- Casks/macports.rb
+++ Casks/macports.rb
@@ -5,7 +5,7 @@ cask "macports" do
     end
   end
 
-  version "2.7.2"
+  version "2.8.1"
   url "https://github.com/macports/macports-base/releases/download/v#{version}/MacPorts-#{version}-#{MacOS.version}-#{Utils.os_name}.pkg",
       verified: "github.com/macports/macports-base/"
   name "MacPorts"
@@ -17,29 +17,37 @@ cask "macports" do
     strategy :github_latest
   end
 
+  on_ventura do
+    sha256 "577512628a4b9237b3eccd0e18af28e06855f5d55bd71957c37a9c7c362de5f3"
+    depends_on macos: ">= :ventura"
+  end
   on_monterey do
-    sha256 "9b9bc7bc552247f4d7dcb217eb5a3fc28f35e824d3769bb64e398f99d43777ea"
+    sha256 "21b2e8e94f04897bee530d871ef83becbb48231a7107ebb9cc0e8a9063881b31"
     depends_on macos: ">= :monterey"
   end
   on_big_sur do
-    sha256 "d2ddb4e7aaa554040ee4761786baeb7a49acf80c30c06b88a79e40ac4f8c5ff8"
+    sha256 "99b8f955948a9ece731f3ad7ffd913e0c11a9f6d4e166949e6ffbad42c04c884"
     depends_on macos: ">= :big_sur"
   end
   on_catalina do
-    sha256 "6a82d1de0fe2db046c82a388063841ff62d6f5616a9058cc7d720515b604b64f"
+    sha256 "f360da9245bf6d124f992d7175d33bbb37eba2dc6c05aa6768460ff998d1e083"
     depends_on macos: ">= :catalina"
   end
   on_mojave do
-    sha256 "d2e1ab88eb27a3e8335e061c056e3ffc95e0934b85e3f1d14eb6f8d44ba0ef29"
+    sha256 "fa9369e05395cedc0540d3f03f89ce9ff226789803e10e99405ab3426624dfde"
     depends_on macos: ">= :mojave"
   end
   on_high_sierra do
-    sha256 "5cc55a1d32dff1a2d20149d2ba5d377018ad4951828b1b869fe90ba4df400eca"
+    sha256 "6f103815539a2b8892a77374589e2acf957cda493329b72bba1d05c7453e1cac"
     depends_on macos: ">= :high_sierra"
   end
   on_sierra do
-    sha256 "74c42e4251799541ef85f29f6811f1d5fa1ae67cb8e318872e83031c8daa9068"
+    sha256 "a33d4e7d113d7fa3bc14c4f874a340edaa0fb437ed90f8e1864986cfc2a73906"
     depends_on macos: ">= :sierra"
+  end
+  on_el_capitan do
+    sha256 "1fba6dae73235383ba87e9f1dc94be34e0e35add40a906d430ba43b250defa79"
+    depends_on macos: ">= :el_capitan"
   end
 
   pkg "MacPorts-#{version}-#{MacOS.version}-#{Utils.os_name}.pkg"

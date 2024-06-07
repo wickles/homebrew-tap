b6b6ad0dc188dfc3934b96ec52b27972089abe04
wickles <wickles137@gmail.com>
2024-06-06 19:07:41 -0700

    macports 2.9.3

--- Casks/macports.rb
+++ Casks/macports.rb
@@ -5,34 +5,34 @@ cask "macports" do
     end
   end
 
-  version "2.8.1"
+  version "2.9.3"
 
   on_el_capitan do
-    sha256 "1fba6dae73235383ba87e9f1dc94be34e0e35add40a906d430ba43b250defa79"
+    sha256 "a2e321d698b3222e81b80b4f5dfc5d23d6d9e6b3f9de05b85bf148a9c93657a3"
   end
   on_sierra do
-    sha256 "a33d4e7d113d7fa3bc14c4f874a340edaa0fb437ed90f8e1864986cfc2a73906"
+    sha256 "41963004bab6b73a00d6861beb083fc4be31e8ea419697e6d232e34d3d5e13a7"
   end
   on_high_sierra do
-    sha256 "6f103815539a2b8892a77374589e2acf957cda493329b72bba1d05c7453e1cac"
+    sha256 "47fdc20239c63e039740dada5749175d788ecdc08061623404a4f5f2a530d114"
   end
   on_mojave do
-    sha256 "fa9369e05395cedc0540d3f03f89ce9ff226789803e10e99405ab3426624dfde"
+    sha256 "a04d3e5b14b929bf7413742de6f0a5927b9a568a1e9d67daf19f67735c5bdb48"
   end
   on_catalina do
-    sha256 "f360da9245bf6d124f992d7175d33bbb37eba2dc6c05aa6768460ff998d1e083"
+    sha256 "566eeab7e4c52fb300063b5a16b20a1a3dfeac4d76b5e894166991ace6285288"
   end
   on_big_sur do
-    sha256 "99b8f955948a9ece731f3ad7ffd913e0c11a9f6d4e166949e6ffbad42c04c884"
+    sha256 "6100cab78063791b409cdaa76de53744fa93f5f26d60878e9c0a757901763aea"
   end
   on_monterey do
-    sha256 "21b2e8e94f04897bee530d871ef83becbb48231a7107ebb9cc0e8a9063881b31"
+    sha256 "20ce1e2a4bb5c30b0d28e9a6e33b86a1cf0a3d9d4d60861e62bf2595c550e4ed"
   end
   on_ventura do
-    sha256 "577512628a4b9237b3eccd0e18af28e06855f5d55bd71957c37a9c7c362de5f3"
+    sha256 "6b826ab7af146b5b7011e2cc1257d27f6133945c90f3a02c150eb1fc96a4669e"
   end
   on_sonoma do
-    sha256 "3f1335efed75d541d6eca3321a3b1af9178f35ddedf585cd3896e298a9aa2a6d"
+    sha256 "b5af4f2a2aca2a5a8896394a3b8967e56c6d59dffd17972b5be0369ad2830eb7"
   end
 
   url "https://github.com/macports/macports-base/releases/download/v#{version}/MacPorts-#{version}-#{MacOS.version}-#{Utils.os_name}.pkg",

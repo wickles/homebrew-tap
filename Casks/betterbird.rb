4dd1bc61456fdded58d0a9f8cfcdd85d918ae256
wickles <wickles137@gmail.com>
2025-08-24 14:08:30 -0700

    betterbird 140.2.0esr-bb9

--- Casks/betterbird.rb
+++ Casks/betterbird.rb
@@ -1,9 +1,9 @@
 cask "betterbird" do
   arch arm: "-arm64"
 
-  version "128.13.0esr-bb31"
-  sha256 arm:          "063b5a8173c24fa72ae1a21e7898f0d999637508af537fb9c2c529db29257f6b",
-         intel:        "0ca5c0e573d761cb9aa1de29c40b3125030a5b6ba00ce214ef6c2190366bfc04",
+  version "140.2.0esr-bb9"
+  sha256 arm:          "db7a8c867f671be43a7130ef1a43517237d6c3acea6567eaf32879680d973646",
+         intel:        "ae8d67545f9455591c8ddd5c921688a1832c36b5671e5b4a0f1b51f42194a0fe",
          arm64_linux:  "0",
          x86_64_linux: "0"
 
@@ -18,8 +18,8 @@ cask "betterbird" do
     strategy :header_match
   end
 
-  # From Thunderbird system requirements.
-  depends_on macos: ">= :sierra"
+  auto_updates true
+  depends_on macos: ">= :catalina"
 
   app "Betterbird.app"
 

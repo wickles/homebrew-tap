6003022fc9d6c681b884dd53d052f9f6f7eec14f
wickles <wickles137@gmail.com>
2026-08-25 20:32:52 -0700

    djview 4.12,3.5.29 (new cask)

---
+++ Casks/djview.rb
@@ -0,0 +1,43 @@
+cask "djview" do
+  version "4.12,3.5.29"
+  sha256 "eebe6373c896a02d102607566dfa0626ab8541b7816779c3bce5524bf10e65fb"
+
+  url "https://downloads.sourceforge.net/djvu/DjVuLibre-#{version.csv.second}%2BDjView-#{version.csv.first}-universal.dmg"
+  name "DjView"
+  desc "DjVu viewer and browser plugin"
+  homepage "https://djvu.sourceforge.net/"
+
+  livecheck do
+    url "https://sourceforge.net/projects/djvu/rss?path=/DjVuLibre_MacOS"
+    regex(%r{url=.*?/DjVuLibre[._-](\d+(?:\.\d+)+)%2BDjView[._-](\d+(?:\.\d+)+)[._-]universal(?:-(\d+))?\.dmg}i)
+    strategy :sourceforge do |page, regex|
+      page.scan(regex).map do |match|
+        match[2].present? ? "#{match[1]},#{match[0]},#{match[2]}" : "#{match[1]},#{match[0]}"
+      end
+    end
+  end
+
+  depends_on :macos
+
+  app "DjView.app"
+
+  postflight do
+    ohai "Releasing #{token} from quarantine"
+    system_command("/usr/bin/xattr",
+                   args: [
+                     "-dr",
+                     "com.apple.quarantine",
+                     "#{appdir}/DjView.app",
+                   ])
+  end
+
+  zap trash: [
+    "~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/org.djvu.djview.sfl*",
+    "~/Library/Preferences/org.djvu.DjView.plist",
+    "~/Library/Saved Application State/org.djvu.DjView.savedState",
+  ]
+
+  caveats <<~EOS
+    Warning: macOS's Gatekeeper has been disabled for this Cask
+  EOS
+end

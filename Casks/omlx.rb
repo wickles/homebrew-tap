fb23df1da8a73b30274df9a289a2a2d8ae3e71d5
wickles <wickles137@gmail.com>
2026-08-28 13:06:50 -0700

    omlx: remove macos15 version due to poor maintainability

--- Casks/omlx.rb
+++ Casks/omlx.rb
@@ -1,19 +1,9 @@
 cask "omlx" do
   version "0.6.3"
+  sha256 "5bde65e35c0cc3e7b0365c0e078f98d7571cb71c6a6bead591329a2cf8287537"
 
-  on_sequoia do
-    sha256 "d56c37e55b17f478f532567016d3b2b325423234727d3036a65b491dd4030a2c"
-
-    url "https://github.com/jundot/omlx/releases/download/v#{version}/oMLX-#{version}-macos15-sequoia.dmg",
-        verified: "github.com/jundot/omlx"
-  end
-  on_tahoe :or_newer do
-    sha256 "5bde65e35c0cc3e7b0365c0e078f98d7571cb71c6a6bead591329a2cf8287537"
-
-    url "https://github.com/jundot/omlx/releases/download/v#{version}/oMLX-#{version}-macos26-27.dmg",
-        verified: "github.com/jundot/omlx"
-  end
-
+  url "https://github.com/jundot/omlx/releases/download/v#{version}/oMLX-#{version}-macos26-27.dmg",
+      verified: "github.com/jundot/omlx"
   name "oMLX"
   desc "MLX server with smart caching"
   homepage "https://omlx.ai/"
@@ -23,6 +13,7 @@ cask "omlx" do
     strategy :git
   end
 
+  # macos26 version defines the minimum OS as macos15
   depends_on macos: :sequoia
 
   app "oMLX.app"
